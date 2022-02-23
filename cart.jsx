73d3628f7713c0e74bb9ec0226bbe998470067cf
kimberlysmith82 <90233407+kimberlysmith82@users.noreply.github.com>
2022-02-22 16:09:46 -0800

    Update cart.jsx

--- cart.jsx
+++ cart.jsx
@@ -116,7 +116,7 @@ const Products = (props) => {
 
   let list = items.map((item, index) => {
     //let n = index + 1049;
-    //let url = "https://picsum.photos/id/" + n + "/50/50";
+    //let url = "https://picsum.photos/50/50/" + n + "/50/50";
 
     return (
       <li key={index}>
